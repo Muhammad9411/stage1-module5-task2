b89bd22a154faa36465f26a34f9a65837a7e2d5a
MuhammadJon <mtjohn9411@gmail.com>
2023-04-07 09:43:25 +0500

    Stage 1 Lambda task has been developed

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,18 @@
 package com.epam.mjc;
 
-public class InterfaceCreator {
+import java.util.ArrayList;
+import java.util.List;
 
+public class InterfaceCreator {
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        return (l) -> {
+            List<Integer> r = new ArrayList<>();
+            for (int i = 0; i < l.size(); i++) {
+                r.add(l.get(i) / divider);
+            }
+            return r;
+        };
+
     }
 }
